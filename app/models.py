8cd2be69724bc799d79d45fc3ca55ee735d2f77f
Sebastian <Sebastian@algirdo.com>
2026-03-12 09:06:51 -0400

    chore: move all dataclasses to models.py

---
+++ app/models.py
@@ -0,0 +1,25 @@
+"""All Dataclass models for the program"""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class SimulationStep:
+
+
+@dataclass
+class SimParam:
+
+@dataclass
+class RiverNode:
+
+
+@dataclass
+class RiverEdge:
+    __init__
+
+@dataclass
+class Cell:
+
+@dataclass
+class Dam:
