15e9c1c383a048c03b594446399023aceb657bd7
Sebastian <Sebastian@algirdo.com>
2026-04-03 11:27:33 -0400

    fix(simulation): correct indentation for dam meadow condition

--- beaver_dam_sim/simulation/simulation.py
+++ beaver_dam_sim/simulation/simulation.py
@@ -108,8 +108,8 @@ class Simulation:
                         lower_cell.flood(self._step)
                         if lower_cell.dam:
                             lower_cell.dam.break_dam(self._step)
-                        if lower_cell.dam.meadow:
-                            lower_cell.dam.meadow.meadow = False
+                            if lower_cell.dam.meadow:
+                                lower_cell.dam.meadow = False
                     # All cells after a cascade break must be flooded and have dams broken
                     break
 
