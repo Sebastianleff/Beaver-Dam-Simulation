a8ea98b2189a06291305bf6f545a02dca43232b2
Sebastian <Sebastian@algirdo.com>
2026-04-13 11:43:55 -0400

    test(models): add unit tests for RiverEdge initialization and cell creation

--- tests/simulation/test_models.py
+++ tests/simulation/test_models.py
@@ -1,10 +1,19 @@
-import unittest
+from unittest import TestCase
+
+from beaver_dam_sim.simulation.models import RiverEdge, RiverNode
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestRiverEdge(TestCase):
+    def test_post_init_creates_cells_with_expected_shape(self):
+        down = RiverNode()
+        up = RiverNode()
 
+        edge = RiverEdge(down_stream_node=down, up_stream_node=up, length=30)
 
-if __name__ == '__main__':
-    unittest.main()
+        self.assertEqual(len(edge.cells), 3)
+        self.assertEqual(list(edge.cells.keys()), [1, 2, 3])
+
+        positions = [cell.position for cell in edge.cells.values()]
+        self.assertEqual(positions, [1, 2, 3])
+
+        self.assertTrue(all(cell.edge is edge for cell in edge.cells.values()))
